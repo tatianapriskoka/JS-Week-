219a3744297236ca9d50ed7728eee81c715fcb2b
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-01-22 07:37:01 +0300

    git branch -M main

---
+++ index.js
@@ -0,0 +1,31 @@
+let usernameInput = document.querySelector('.username__input');
+let userpicInput = document.querySelector('.userpic__input');
+let textInput = document.querySelector('.text__input');
+let button = document.querySelector('.button');
+let chatHidden = document.querySelector('.block-hidden');
+let username = document.querySelector('.block-hidden__username');
+let comment = document.querySelector('.block-hidden__comment');
+let image = document.querySelector('.image');
+
+console.log(comment);
+
+
+button.addEventListener('click', showComment);
+function showComment() {
+    image.src = userpicInput.value;
+    let usernameWords = usernameInput.value;
+    let usernameLower = usernameWords.toLowerCase();
+    let usernameChanged = usernameLower.trim();
+    function capitalizeWords(usernameChanged) {
+        return usernameChanged.replace(/(^|\s)\S/g, function (upperCase) { return upperCase.toUpperCase() })
+    }
+    username.textContent = capitalizeWords(usernameChanged);
+    chatHidden.style.display = 'block';
+    let commentWords = textInput.value;
+    let spam = commentWords.replace(/viagra/ig, "***");
+    comment.textContent = spam;
+
+    button.addEventListener('click', function (event) {
+        event.preventDefault();
+    });
+}
