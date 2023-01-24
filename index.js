97c6c93a7bd693b5578732aa331b7a0cc6b602ab
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-01-24 19:55:51 +0300

    fixed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let usernameInput = document.querySelector('.username__input');
-let userpicInput = document.querySelector('.userpic__input');
-let textInput = document.querySelector('.text__input');
-let button = document.querySelector('.button');
-let chatHidden = document.querySelector('.block-hidden');
-let username = document.querySelector('.block-hidden__username');
-let comment = document.querySelector('.block-hidden__comment');
-let image = document.querySelector('.image');
+const usernameInput = document.querySelector('.form__input');
+const userpicInput = document.querySelector('.userpic__input');
+const textInput = document.querySelector('.text__input');
+const button = document.querySelector('.button');
+const chatHidden = document.querySelector('.block-hidden');
+const username = document.querySelector('.block-hidden__username');
+const comment = document.querySelector('.block-hidden__comment');
+const image = document.querySelector('.image');
 
 button.addEventListener('click', showComment);
 function showComment() {
@@ -13,9 +13,6 @@ function showComment() {
     let usernameWords = usernameInput.value;
     let usernameLower = usernameWords.toLowerCase();
     let usernameChanged = usernameLower.trim();
-    function capitalizeWords(usernameChanged) {
-        return usernameChanged.replace(/(^|\s)\S/g, function (upperCase) { return upperCase.toUpperCase() })
-    }
     username.textContent = capitalizeWords(usernameChanged);
     chatHidden.style.display = 'block';
     let commentWords = textInput.value;
@@ -25,4 +22,7 @@ function showComment() {
     button.addEventListener('click', function (event) {
         event.preventDefault();
     });
+}
+function capitalizeWords(usernameChanged) {
+    return usernameChanged.replace(/(^|\s)\S/g, function (upperCase) { return upperCase.toUpperCase() })
 }
